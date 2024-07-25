88873c2e91b36753e7590a6f9f1800b18582e0ce
ayusharyan143 <ayusharyan686@gmail.com>
2024-07-25 21:48:45 +0530

    developed some  edit delet completed button

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 function loadTodos() 
 {
   // This function will load the todos from the browser
@@ -10,15 +11,20 @@ function loadTodos()
 
 }
 
+function refreshTodos(todos)
+{
+    localStorage.setItem("todos" , JSON.stringify(todos) )
+}
+
 
 
 // Function to add todo to local storage
-function addTodoToLocalStorage(todoText) 
+function addTodoToLocalStorage(todo) 
 {
 
     const todos = loadTodos();
 
-    todos.todoList.push(todoText);
+    todos.todoList.push({...todo });
 
     localStorage.setItem("todos", JSON.stringify(todos));
 
@@ -26,20 +32,166 @@ function addTodoToLocalStorage(todoText)
 
 
 
+function executeFilterAction(event)
+{
+    const todoList = document.getElementById("todoList");
+    const element = event.target ; 
+    const value =  element.getAttribute("data-filter");
+   
+    todoList.innerHTML = '';
+    const todos = loadTodos();
 
-function appendTodoInhtml(todoText) 
+
+
+    if( value == 'all')
+    {
+        console.log(todoList);
+
+        todos.todoList.forEach((todo) => {
+            appendTodoInhtml(todo)
+        });
+
+        
+    }
+    else if(value == 'pending')
+    {
+        todos.todoList.forEach((todo) => {
+
+            if(todo.isCompleted != true)
+            {
+                appendTodoInhtml(todo)
+            }
+        });
+
+    }
+    else
+    {
+        todos.todoList.forEach((todo) => {
+
+            if(todo.isCompleted == true)
+            {
+                appendTodoInhtml(todo)
+            }
+        });
+    }
+}
+
+
+
+function appendTodoInhtml(todo)
 {
 
     const todoList  = document.getElementById("todoList")
 
-    const todo = document.createElement("li");
+    const todoItem = document.createElement("li");
 
-    todo.textContent = todoText; // Add the todo text to the list item
+    todoItem.setAttribute("data-id" , todo.id)
 
-    todoList.appendChild(todo);
+    const textDiv = document.createElement("div");
+
+    if( todo.isCompleted)
+    {
+        textDiv.classList.add("completed")
+    }
+
+
+    textDiv.textContent = todo.text; 
+    todoItem.classList.add("todoItem")
+
+
+    const wrapper = document.createElement("div")
+    wrapper.classList.add("todoButtons")
+
+
+    const editBtn = document.createElement("button");
+    editBtn.textContent = "Edit";
+    editBtn.classList.add("editBtn");
+    editBtn.addEventListener("click" , editTodo );
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "Delete";
+    deleteBtn.classList.add("deleteBtn");
+    deleteBtn.addEventListener("click",deleteTodo);
+
+    const completedBtn = document.createElement("button");
+    completedBtn.textContent = (todo.isCompleted) ? "Reset" : "Completed";
+    completedBtn.classList.add("completedBtn");
+    completedBtn.addEventListener("click" , toggleTodo);
+
+    wrapper.appendChild(editBtn)
+    wrapper.appendChild(deleteBtn)
+    wrapper.appendChild(completedBtn)
+
+    todoItem.appendChild(textDiv);
+    todoItem.appendChild(wrapper);
+
+    todoList.appendChild(todoItem);
 
 }
 
+
+
+function resetHtmlTodos(todos)
+{
+    const todoList = document.getElementById("todoList");
+    todoList.innerHTML = '';
+    
+    todos.todoList.forEach( todo => {
+        appendTodoInhtml(todo);
+    });
+
+}
+
+function toggleTodo(event)
+{
+    const todoItem = event.target.parentElement.parentElement ; 
+    const todoId = todoItem.getAttribute("data-id");
+    const todos = loadTodos();
+
+    todos.todoList.forEach( todo => {
+        if(todo.id == todoId )
+        {
+            todo.isCompleted = !todo.isCompleted ; 
+        }
+    });
+
+    refreshTodos(todos);
+    resetHtmlTodos(todos);
+    
+}
+
+
+function deleteTodo(event)
+{
+    const todoItem = event.target.parentElement.parentElement ; 
+    const todoId = todoItem.getAttribute("data-id");
+    let todos = loadTodos();
+
+    todos.todoList =  todos.todoList.filter( todo => todo.id != todoId );
+    refreshTodos(todos);
+    resetHtmlTodos(todos);
+
+}
+
+
+function editTodo(event)
+{
+    const todoItem = event.target.parentElement.parentElement ; 
+    const todoId = todoItem.getAttribute("data-id");
+    let todos = loadTodos();
+
+    const response = prompt("What is the new todo value you want to set ? ");
+    todos.todoList =  todos.todoList.filter( todo => {
+        if( todo.id == todoId )
+        {
+            todo.text = response ; 
+        }
+    });
+    refreshTodos(todos);
+    resetHtmlTodos(todos);
+
+
+}
 
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -49,32 +201,47 @@ document.addEventListener("DOMContentLoaded", () => {
   
     const submitButton = document.getElementById("addTodo");
 
-    const todoList  = document.getElementById("todoList")
+    let todos = loadTodos();
 
 
+    const todoList  = document.getElementById("todoList");
+
+
+
+    const filterBtns = document.getElementsByClassName("filterBtn");
+
+    for( const btn of filterBtns )
+    {
+        btn.addEventListener("click" , executeFilterAction);
+    }
+
+
+
+function addNewTodo()
+{
+     
+    const todoText = todoInput.value;
     
+    if (todoText === "") 
+    {
+        alert("Please write something for the todo!");
+    } 
+    else 
+    {
+        todos = loadTodos();
+        const id = todos.todoList.length ; 
+        addTodoToLocalStorage({text: todoText , isCompleted: false , id});
 
-  
-    submitButton.addEventListener("click", (event) => {
+        appendTodoInhtml({text: todoText , isCompleted: false , id});
+        
+        todoInput.value = '';
+        
+    }
+}
 
-    
-        const todoText = todoInput.value;
-    
-        if (todoText === "") 
-        {
-            alert("Please write something for the todo!");
-        } 
-        else 
-        {
-            addTodoToLocalStorage(todoText);
 
-            appendTodoInhtml(todoText);
-            
-            todoInput.value = '';
-            
-        }
 
-  });
+ submitButton.addEventListener("click", addNewTodo );
 
 
   todoInput.addEventListener("change", (event) => {
@@ -91,17 +258,17 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
   // Load and display todos when the page loads
-  const todos = loadTodos();
-
   todos.todoList.forEach((todo) => 
   {
-    const newTodoItem = document.createElement("li")
-    
-    newTodoItem.textContent = todo ; 
-
-    todoList.appendChild(newTodoItem);
-
-    
+    appendTodoInhtml(todo)
    });
+  
+
+   document.addEventListener("keypress" , (event) => {
+        if(event.code === 'Enter')
+        {
+            addNewTodo();
+        }
+   })
 
 });
